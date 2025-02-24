d942830ebc4caf2fdec996f0200b1d3cdf0910a7
r1ndousan <rassengansan@mail.ru>
2025-02-24 23:15:21 +0300

    /2

--- 1lab/1lab/_1lab.py
+++ 1lab/1lab/_1lab.py
@@ -1,15 +1,24 @@
 import math
 
-a = input("Длина первого катета: ")
-b = input("Длина второго катета: ")
+while True:
+    try:
+        a = int(input("Длина первого катета: "))
+        b = int(input("Длина второго катета: "))
 
-a = float(a)
-b = float(b)
+        a = float(a)
+        b = float(b)
+        break
 
+    except ValueError:
+        print("Вы не ввели число. Попробуйте ещё раз.")
+    
 c = math.sqrt(a ** 2 + b ** 2)
 
 S = a * b / 2
 P = a + b + c
 
 print(f"Площадь треугольника: {S:.2f}")
-print(f"Периметр треугольника: {P:.2f}")
+print(f"Периметр треугольника: {P:.2f}")    
+
+
+
